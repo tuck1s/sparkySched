a78c833819f0d9a125771f957a08501713f844fd
tuck1s <steve.tuck@messagesystems.com>
2017-03-20 19:21:21 +0000

    Uses Batch mode API calls to efficiently send mail.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # sparkySched   
-Simple command-line tool to make scheduled transmissions through SparkPost.
+Simple command-line tool to make scheduled transmissions through SparkPost using a stored template, with
+- recipient list
+- template ID
+- sending time
+
+specified through parameters.
 
 ## Pre-requisites
 Install the [python-sparkpost](https://github.com/sparkpost/python-sparkpost) library using
@@ -11,44 +16,74 @@ Set up a sparkpost.ini file as follows.
   
 ```
 [SparkPost]
-# SparkPost.com values
-Authorization = "##myAPIkey##"
-```
-
-or
-
-```
-[SparkPost]
-# SparkPost Enterprise values
+# SparkPost Enterprise EU values
 Authorization = "##myAPIkey##"
 Host = "demo.sparkpostelite.com"
+Return-Path = "bounces@elite.trysparkpost.com"
+Binding = "outbound"
+BatchSize = "10000"
+Campaign = "Daily send test 10k batches"
 ```
-
-The `host` attribute is only needed for SparkPost Enterprise service usage; you can omit it for [sparkpost.com](https://www.sparkpost.com/).
 
 The surrounding quotes are needed.
 
-Replace ##myAPIkey## with your specific, private API key. 
+Replace `##myAPIkey##` with your specific, private API key. 
 
-##Usage
-As written, the tool injects serially 1000 messages, with a start time scheduled for 300 seconds into the future.
+The `host`, `Return-Path` and `Binding` attributes are only needed for SparkPost Enterprise service usage; you can omit them for [sparkpost.com](https://www.sparkpost.com/).
 
-The messages are destined for the [Smart Sink](https://support.sparkpost.com/customer/portal/articles/2560839-how-do-i-test-using-the-sink-server-on-sparkpost-).  They will show up as actual deliveries in your account (because they are actually getting delivered; and they count towards your account volume allowance).
+The `BatchSize` attribute can be left at 10000, the Best Practices article below discusses this.
+
+The `Campaign` attribute is free format text and is helpful for filtering reports on the UI.
+
+The tool injects messages using the specified local recipients file and existing SparkPost stored template (referred to by template_ID),  with a scheduled start time.
+
+An example recipients file that is destined for the [Smart Sink](https://support.sparkpost.com/customer/portal/articles/2560839-how-do-i-test-using-the-sink-server-on-sparkpost-) is included for test purposes.
+They will show up as actual deliveries in your account (because they are actually getting delivered; and they count towards your account volume allowance).
+
+## Usage
 ```
 $ ./sparkySched.py 
-Scheduled send:  2017-03-05T14:24:55  UTC to service:  https://demo.sparkpostelite.com
-Message injected: ID=48513559285872950
-Message injected: ID=66527959062029757
-Message injected: ID=30499159685870927
-Message injected: ID=48513559285872951
-:
-:
+
+NAME
+   ./sparkySched.py
+   Simple command-line tool to trigger bulk sends through SparkPost using a stored template.
+
+SYNOPSIS
+  ./sparkySched.py recipient_list template_id sending_time
+
+MANDATORY PARAMETERS
+    recipient_list       filename, containing plain text list of email addresses
+    template_id          SparkPost named template
+    sending_time         In ISO format, interpreted as UTC (Greenwich Mean Time) as in YYYY-MM-DDTHH:MM:SS
+                             with optional timezone offset as in YYYY-MM-DDTHH:MM:SS±HH:MM
+```
+
+## Example output
+
+```
+$ ./sparkySched.py recips_100k.csv west-wing-dry-run 2017-03-20T19:10:00+00:00
+Opened connection to https://demo.sparkpostelite.com
+Injecting to SparkPost:
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 66538125845299279 in 13.311 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 48523727151264958 in 14.656 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 66538125845299281 in 12.074 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 48523727151264960 in 15.122 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 30509328364958224 in 10.332 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 48523727151264994 in 15.058 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 30509328364958227 in 9.992 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 48523727151265006 in 39.808 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 30509328364958273 in 12.413 seconds
+To 10000 recips: template "my-dry-run" binding "outbound" campaign "Daily send test 10k batches" start_time 2017-03-20T19:10:00+00:00 : OK TxID 66538125845299402 in 10.705 seconds
+$
 ```
 
 ##Performance considerations
 You'll get the very best performance if you are able to host the tool "close" to the SparkPost injection point i.e. low latency.
 
 A real production injector should also make use of concurrency - see [best practices](https://support.sparkpost.com/customer/portal/articles/2249268), but please note this code is deliberately sending single-recipient-per-call.
+
+## TODO / possible extensions
+Global substitution data / metadata, and per-recipient data / metadata could be included via extensions to the command-line parameters and recipients file format.
 
 ##See Also
 
--- sparkySched.py
+++ sparkySched.py
@@ -15,11 +15,11 @@
 #limitations under the License.
 
 #
-# Author: Steve Tuck (January 2017)
+# Author: Steve Tuck (March 2017)
 #
 
 from __future__ import print_function
-import configparser
+import configparser, time, json, sys, os
 from sparkpost import SparkPost
 from datetime import datetime,timedelta
 from sparkpost.exceptions import SparkPostAPIException
@@ -30,35 +30,111 @@ def stripQuotes(s):
         s = s[1:-1]
     return s
 
+def printHelp():
+    progName = sys.argv[0]
+    shortProgName = os.path.basename(progName)
+    print('\nNAME')
+    print('   ' + progName)
+    print('   Simple command-line tool to trigger bulk sends through SparkPost using a stored template.\n')
+    print('SYNOPSIS')
+    print('  ./' + shortProgName + ' recipient_list template_id sending_time\n')
+    print('MANDATORY PARAMETERS')
+    print('    recipient_list       filename, containing plain text list of email addresses')
+    print('    template_id          SparkPost named template')
+    print('    sending_time         In ISO format, interpreted as UTC (Greenwich Mean Time) as in YYYY-MM-DDTHH:MM:SS')
+    print('                             with optional timezone offset as in YYYY-MM-DDTHH:MM:SS±HH:MM')
+
+# Check/enforce SparkPost's native start_time format, which is slightly different to Python's
+def isExpectedDateTimeFormat(timestamp):
+    format_string = '%Y-%m-%dT%H:%M:%S%z'
+    try:
+        colon = timestamp[-3]
+        if not colon == ':':
+            raise ValueError()
+        colonless_timestamp = timestamp[:-3] + timestamp[-2:]
+        datetime.strptime(colonless_timestamp, format_string)
+        return True
+    except ValueError:
+        return False
+
+# Inject the messages into SparkPost for a batch of recipients, using a named template, binding, and campaign ID
+# Global substitution data is applied, along with a start_time for scheduled sending.
+def sendToRecips(sp, recipBatch, template, binding, campaign, globalSubData, returnPath, startTime):
+    print('To', str(len(recipBatch)).rjust(5, ' '),
+          'recips: template "' + template + '" binding "' + binding + '" campaign "' + campaign + '" start_time ' + startTime + ' : ',
+          end='', flush=True)
+
+    sendObj = dict(
+        recipients=recipBatch,
+        template=template,  # this sets the friendly-from and subject line
+        track_opens=True,
+        track_clicks=True,
+        use_draft_template=False,
+        start_time=startTime,
+        campaign=campaign,  # Note attribute is called campaign, not campaign_id
+        return_path=returnPath,
+        metadata={"binding": binding},
+        substitution_data=globalSubData
+    )
+    startT = time.time()
+    try:
+        response = sp.transmissions.send(**sendObj)
+        endT = time.time()
+        print('OK TxID', response['id'], 'in', round(endT - startT, 3), 'seconds')
+    except SparkPostAPIException as err:
+        print('error code', err.status, ':', err.errors)
+        exit(1)
+
+# -----------------------------------------------------------------------------------------
+# Main code
+# -----------------------------------------------------------------------------------------
+# Get parameters from .ini file
 # Get connection parameters from .ini file
 config = configparser.ConfigParser()
 config.read('sparkpost.ini')
-apiKey = stripQuotes(config['SparkPost']['Authorization'])
-uri = 'https://' + stripQuotes(config['SparkPost']['Host'])
-sp = SparkPost(apiKey, uri)
-
-# Schedule this send for N seconds in the future
-st = datetime.utcnow()+timedelta(seconds=300)
-stStr = st.strftime('%Y-%m-%dT%H:%M:%S')
-print('Scheduled send: ', stStr, ' UTC to service: ', uri)
-
-sendObj = dict(
-    recipients = ['test@sparkysched.sink.sparkpostmail.com'],
-    html = '<p>Hello world</p>',
-    from_email = 'stevet-test@elite.trysparkpost.com',
-    subject = 'This is a scheduled transmission sent via python-sparkpost',
-    track_opens = True,
-    track_clicks = True,
-    start_time = stStr,
-    campaign = 'sparkySched Test',              # Note attribute is called campaign, not campaign_id
-    return_path =  'bounces@elite.trysparkpost.com',
-    metadata = {"binding":  "outbound"}
+p = dict(
+    apiKey = stripQuotes(config['SparkPost']['Authorization']),
+    uri = 'https://' + stripQuotes(config['SparkPost']['Host']),
+    binding = stripQuotes(config['SparkPost']['Binding']),
+    returnPath = stripQuotes(config['SparkPost']['Return-Path']),
+    batchSize = int(stripQuotes(config['SparkPost']['BatchSize'])),
+    campaign  = stripQuotes(config['SparkPost']['Campaign']),
 )
 
-for i in range(0,1000):
+# Get the recipients, today's deals, and sending time from the command-line
+# Check argument count and validate command-line input.  If a file cannot be opened, then Python will raise an exception
+if len(sys.argv) >= 4:
     try:
-        response = sp.transmissions.send(**sendObj)
-        print('Message injected: ID='+response['id'])
-    except SparkPostAPIException as err:
-        print('SparkPost returned error code', err.status, ':',err.errors)
+        fh_recipList = open(sys.argv[1])
+    except FileNotFoundError as Err:
+        print('Error opening recipients file', Err.filename, ':', Err.strerror)
         exit(1)
+
+    # Get today's selected deal IDs as integers
+    templateID = sys.argv[2]
+
+    timeString = sys.argv[3]
+    if(not isExpectedDateTimeFormat(timeString)):
+        print('Unexpected date/time string value', timeString)
+        exit(1)
+else:
+    printHelp()
+    exit(0)
+
+# Open the 'adapter' towards SparkPost
+sp = SparkPost(p['apiKey'], p['uri'])
+print('Opened connection to',p['uri'])
+
+# Read the recipients from the file specified on the command line.  Build them into batches of N
+print('Injecting to SparkPost:')
+gs = {}                                         # Fully formed template - no global substitution data required
+recipBatch = []
+for r in fh_recipList:
+    recipBatch.append(r.rstrip() )              # strip the trailing newline character
+    if(len(recipBatch) >= p['batchSize'] ):
+        sendToRecips(sp, recipBatch, templateID, p['binding'], p['campaign'], gs, p['returnPath'], timeString)
+        recipBatch = []                         # Empty out, ready for next batch
+
+# Handle the final batch remaining, if any
+if(len(recipBatch) > 0):
+    sendToRecips(sp, recipBatch, templateID, p['binding'], p['campaign'], gs, p['returnPath'], timeString)
